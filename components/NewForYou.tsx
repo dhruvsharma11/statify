a4ca6a82b581ef03b8048ecf71aac308d15a49be
g <62965101+gavneetb@users.noreply.github.com>
2022-09-08 19:32:45 -0400

    Add files via upload

--- components/NewForYou.tsx
+++ components/NewForYou.tsx
@@ -1,18 +1,46 @@
-import { blue } from "@mui/material/colors";
-import { fontSize } from "@mui/system";
 import React from "react";
 import styles from "../styles/NewForYou.module.css";
+import Image from "next/image";
+import albumImage from "../public/swiftAlbum.png";
 
 const NewForYou = () => {
   return (
     <div className={styles.newforyou}>
-      <div className={styles.title}>
-        <p style={{ fontSize: 20, fontWeight: "bolder" }}>new for you:</p>
-        <p style={{ paddingLeft: 2 }}>check out |artist's name|</p>
-        <p style={{ paddingLeft: 2 }}>single: |song name|</p>
-        <button type="button" className="btn btn-dark">
-          listen now
-        </button>
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <p
+            style={{
+              fontSize: 20,
+              fontWeight: "bolder",
+              paddingLeft: 10,
+              paddingTop: 10,
+            }}
+          >
+            new for you:
+          </p>
+          <p style={{ paddingLeft: 20 }}>check out |artist's name|</p>
+          <p style={{ paddingLeft: 20 }}>single: |song name|</p>
+          <button
+            type="button"
+            className="btn btn-dark"
+            style={{ width: 200, marginLeft: 10 }}
+          >
+            listen now
+          </button>
+        </div>
+        <div className={styles.column}>
+          <Image
+            src={albumImage}
+            style={{
+              paddingLeft: 20,
+              paddingTop: 20,
+              paddingBottom: 20,
+              paddingRight: 20,
+            }}
+            width="50"
+            height="200"
+          />
+        </div>
       </div>
     </div>
   );
--- components/Weekly.tsx
+++ components/Weekly.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import styles from '../styles/weekly.module.css'
-
+import React from "react";
+import styles from "../styles/weekly.module.css";
 
 const LoginBox = () => {
   return (
-<div >
+    <div>
+      <p className={styles.weeklyone}>
+        {" "}
+        <span className={styles.bigger}>11</span> different artists
+      </p>
+      <p className={styles.weeklytwo}>
+        {" "}
+        <span className={styles.bigger}>4</span> different genres
+      </p>
+      <p className={styles.weeklythree}>
+        {" "}
+        <span className={styles.bigger}>0</span> different podcasts
+      </p>
+    </div>
+  );
+};
 
-  <p className = {styles.weeklyone}> <span className='bigger'>11</span> different artists</p>
-  <p className = {styles.weeklytwo}> <span className='bigger'>4</span> different genres</p>
-  <p className = {styles.weeklythree}> <span className='bigger'>0</span> different podcasts</p>
-</div>
-
-
-
-    
-  )
-}
-
-export default LoginBox
+export default LoginBox;
